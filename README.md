df54d8012f0b3793f31746414f88dd7a89d5756f
sahil-s-246 <sahilsrinivas3@gmail.com>
2024-09-15 00:01:38 +0530

    Initial commit:

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Dish Recommender
 
-- Connect to a weaviate collection in your weaviate cloud's sandbox, in this case FoodRecommend.
+- Connect to a weaviate collection in your weaviate cloud's sandbox, in this case RAGi
     There are two steps in this recommendation
      - Retrieval: Semantically search through the data wrt the query
      - Ranking: Use Gemini Flash API to rank the data
--- query.py
+++ query.py
@@ -3,21 +3,46 @@ import streamlit as st
 import weaviate
 import google.generativeai as genai
 import random
-from PIL import Image
-import requests
-import io
+import ast
 
 from weaviate.exceptions import WeaviateQueryError
 
-# Example Query
-# query= "Veg Indian Dishes"
 genai.configure(api_key=st.secrets["API_KEY"])
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
-st.title("Restaurant from Novigrad")
-with open("data.json", "r") as f:
-    menu = json.load(f)
-st.dataframe(menu)
+with open("plan.json", 'r') as f:
+    plans = json.load(f)
+
+st.title("RAGi - The Meal Plan Recommender")
+
+
+def fill_form():
+    # Define options for select boxes
+    status = False
+    genders = ["Male", "Female", "Other"]
+    activity_levels = ["Low", "Moderate", "High", "Active"]
+    dietary_preferences = ["Vegetarian", "Non-Vegetarian", "Vegan", "Pescatarian", "Keto"]
+    health_goals = ["Weight Loss", "Muscle Gain", "Weight Maintenance", "Heart Health", "Fat Loss"]
+
+    # Create the form
+    with st.form(key='user_form', border=False,):
+        age = st.number_input("age", min_value=0, max_value=120)
+        gender = st.selectbox("gender", genders, index=None)
+        weight = st.number_input("Weight (kg)", min_value=0.0,value=50.0)
+        height = st.number_input("Height (cm)", min_value=100)
+        activity_level = st.selectbox("Activity Level", activity_levels,index=None)
+        dietary_preference = st.selectbox("Dietary Preference", dietary_preferences,index=None)
+        health_goal = st.selectbox("Health Goal", health_goals,index=None)
+
+        submit_button = st.form_submit_button(label='Submit')
+
+    if submit_button:
+        # Concatenate values into a space-separated string
+        status = True
+        user_info = f"{age} {gender} {weight} {height} {activity_level} {dietary_preference} {health_goal} "
+        return user_info, status
+    else:
+        return "", status
 
 
 def extract_features(query_result):
@@ -25,11 +50,15 @@ def extract_features(query_result):
     extracted_data = {}
     for i, item in enumerate(query_result.objects):
         properties = item.properties
-        extracted_data[item.properties["dish"]] = {
-            'cuisine': properties.get('cuisine', ''),
-            'category': properties.get('category', ''),
-            'description': properties.get('description', ''),
-            'allergy': properties.get('allergy', '')
+        extracted_data[item.properties["mealPlan"]] = {
+            'UserID': properties.get('userID', ''),
+            'Age': properties.get('age', ''),
+            'Gender': properties.get('gender', ''),
+            'Weight': properties.get('weight', ''),
+            'Height': properties.get('height', ''),
+            'ActivtyLevel': properties.get('activityLevel', ''),
+            'DietaryPreference': properties.get('dietaryPreference', ''),
+
         }
     return extracted_data
 
@@ -38,20 +67,20 @@ def ask_ai_for_recommendations():
     """Prompt AI and gemini will retrieve and rank dishes"""
     ask_ai = st.text_input("Ask AI🤖")
     if ask_ai:
-        res = model.generate_content([f"Recommend some dishes along with description, allegies etc. from the menu: "
-                                      f"{menu}, \n\n ranked by relevance, according to the prompt: "
-                                      f" {ask_ai} Eg. If the prompt is something like a mildly spicy gravy dish with"
-                                      f" paneer then recommend paneer butter masala"])
+        res = model.generate_content([f"Give a meal plan from {plans}"
+                                      f"according to the prompt: "
+                                      f" {ask_ai} In the format breakfast lunch dinner and optional random juice/snacks"])
         st.write(res.text)
 
 
-def recommend(query):
+def recommend():
+
     """Connect to a weaviate collection in your weaviate cloud's sandbox, In this case FoodRecommend
     There are two steps in this recommendation \n
     Retrieval: Semantically search through the data wrt the query \n
     Ranking: Use Gemini Flash API to rank the data"""
 
-    result = first = {}
+    res = ""
     client = weaviate.connect_to_wcs(
         cluster_url=st.secrets["CLUSTER_URL"],
         auth_credentials=weaviate.auth.AuthApiKey(st.secrets["Weav_API_KEY"]),
@@ -62,8 +91,11 @@ def recommend(query):
     )
 
     try:
-        questions = client.collections.get("FoodRecommend")
+        questions = client.collections.get("MealRAGi")
         # Retrieval
+        query, status = fill_form()
+        if not status:
+            return "Submit your data please📚"
         response = questions.query.near_text(
             query=query,
             limit=10
@@ -73,24 +105,17 @@ def recommend(query):
             json.dump(data, file, indent=4)
         # Ranking
 
-        res = model.generate_content([f"Rerank json objects in this data {data} \n\n according to the query: {query}\n\n"
-                                      f"Remove the most irrelevant ones but dont remove many"],
+        res = model.generate_content([f"We have to suggest a meal plan for the user."
+                                      f"Rerank the data\n\n{data} \n\n according to the query: {query}\n\n"
+                                      f"Accordingly provide the most suitable meal plan in the following format:"
+                                      f"\n {plans[list(data.keys())[0]]}"],
                                      generation_config={"response_mime_type": "application/json"})
-        # print(res.text)
-        result = json.loads(res.text)
-        first = result[list(result.keys())[0]]
 
     except WeaviateQueryError:
-        st.write("An error occurred. Please try again. Sorry, your tastes to complex to be catered for by us😝")
+        st.warning("An error occurred. Please try again. Sorry, your tastes to complex to be catered for by us😝")
     finally:
         client.close()  # Close client gracefully
-    return result, first
-
-
-def give_random():
-    """Feeling Lucky? Get a Random dish!"""
-    dish = random.choice(list(menu))
-    return dish
+    return res.text
 
 
 st.write("Enter your choice:")
@@ -101,37 +126,18 @@ if choice is None:
         "Custom": "Dishes will be retrieved according to your query from weaviate's db",
         "ask AI": "Dishes will be recommended by gemini according to your prompt"
     })
-    st.info("Please describe your preference eg: Indian or Japanese, Veg or Non-Veg, Sweet or Spicy etc. "
-            "Or Click 'I'm Feeling Lucky' for a random suggestion")
-    st.warning("Rn there are only 2 cuisines : Indian and Japanese😅")
+
 
 elif choice == "Custom":
-    q = st.text_input("What would you like to have🍉", placeholder="Indian sweet dish")
-    if q:
-        order, first = recommend(q)
-        # Generate Image of first recommendation
-        API_URL = "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4"
-        headers = {"Authorization": st.secrets["hftoken"]}
-        payload = {"inputs": first["description"]}
-        response = requests.post(API_URL, headers=headers, json=payload)
-        image_bytes = response.content
-        name_of_first = list(order.keys())[0]
-        order.pop(name_of_first)
-        with st.spinner("Loading..."):
-            image = Image.open(io.BytesIO(image_bytes))
-            st.write("Recommended for You: ")
-            st.markdown(f"#### :orange[{name_of_first}]")
-            st.write(first)
-            st.image(image)
-        "You might also like:"
 
-        st.write(order)
-        st.warning("Some Recommendations may be inaccurate or irrelevant")
+    order = recommend()
+
+    st.success(f"{order}")
+
 
 elif choice == "ask AI":
     ask_ai_for_recommendations()
 
-click = st.button("I'm Feeling Lucky✨")
-if click:
-    menu_item = give_random()
-    st.write(menu_item)
+rand = st.button("I'm Feeling Lucky✨")
+if rand:
+    st.write(plans[random.choice(list(plans.keys()))])
